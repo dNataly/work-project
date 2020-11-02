b292f43f64c903e705e1cff25e9def45b8df1666
natalia_denysenko <natalia.denysenko@mgid.com>
2020-11-02 18:59:00 +0200

    adaptivity

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@
 //       });
 //     });
 //   });
+
+$(function () { 
+    var header = $(".fixed_button"); 
+    $(window).scroll(function () { 
+        var scroll = $(window).scrollTop(); 
+
+        if (scroll >= 6450) { 
+            header.removeClass('fixed_button') 
+                    .addClass("hide_button"); 
+        } else { 
+            header.removeClass("hide_button") 
+                    .addClass('fixed_button'); 
+        } 
+    }); 
+}); 
